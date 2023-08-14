81f242f250a8800ef657313344f1b6d3ce3aa55a
SatyVardhan <adhurthipsv@.com>
2023-08-14 11:40:42 +0000

    doubt

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+  //   Write your code here.
+
+  <div classList="bg_container">
+    <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"></img>
+    <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png"></img>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
